2fc1d46760a2d4ae6ce0cc9230a0ace8f63ef080
roniya007 <roni.l@outlook.com>
2026-05-23 16:59:41 +0300

    Fix GitHub Pages deployment permissions

--- src/api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bb4e40dae866546bacd2cb63bd2205b398b29e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882f0cb9d33c761f3ef94e0bbdcefad960bb3092")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
